a8977c18ea36a8330cdf7057f188951829787928
Arina Vasiljeva <mistclick.a@gmail.com>
2024-11-28 00:26:54 +0100

    created a basic DB schema for managing users and accounts

--- apps/web/src/server/db/schema.ts
+++ apps/web/src/server/db/schema.ts
@@ -1,7 +1,8 @@
 // Example model schema from the Drizzle docs
 // https://orm.drizzle.team/docs/sql-schema-declaration
 
-import { pgTableCreator } from "drizzle-orm/pg-core";
+import { pgTableCreator, text, timestamp, varchar } from "drizzle-orm/pg-core";
+import { createId } from "@paralleldrive/cuid2";
 
 /**
  * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
@@ -10,3 +11,53 @@ import { pgTableCreator } from "drizzle-orm/pg-core";
  * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
  */
 export const createTable = pgTableCreator((name) => `golive_${name}`);
+
+export const users = createTable("users", {
+  id: varchar("id", { length: 255 })
+    .$defaultFn(() => createId())
+    .primaryKey(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at").notNull().defaultNow(),
+
+  // user details
+  name: varchar("name", { length: 255 }).notNull(),
+  email: varchar("email", { length: 255 }).notNull().unique(),
+  emailVerified: timestamp("email_verified"),
+  image: text("image"),
+});
+
+export const accounts = createTable("accounts", {
+  id: varchar("id", { length: 255 })
+    .$defaultFn(() => createId())
+    .primaryKey(),
+  userId: varchar("user_id", { length: 255 })
+    .notNull()
+    .references(() => users.id),
+  accountId: varchar("account_id", { length: 255 }).notNull().unique(),
+  providerId: varchar("provider_id", { length: 255 }).notNull(),
+  accessToken: varchar("access_token", { length: 255 }),
+  refreshToken: varchar("refresh_token", { length: 255 }),
+  accessTokenExpiresAt: timestamp("access_token_expires_at"),
+  refreshTokenExpiresAt: timestamp("refresh_token_expires_at"),
+  scope: varchar("scope", { length: 255 }),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at").notNull().defaultNow(),
+});
+
+export const sessions = createTable("sessions", {
+  id: varchar("id", { length: 255 })
+    .$defaultFn(() => createId())
+    .primaryKey(),
+  userId: varchar("user_id", { length: 255 })
+    .notNull()
+    .references(() => users.id),
+  token: varchar("token", { length: 255 })
+    .$defaultFn(() => createId())
+    .notNull()
+    .unique(),
+  expiresAt: timestamp("expires_at").notNull(),
+  ipAddress: varchar("ip_address", { length: 255 }),
+  userAgent: varchar("user_agent", { length: 255 }),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at").notNull().defaultNow(),
+});
