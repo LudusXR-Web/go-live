65afae9b3efa3b7208ff0c022372b01e038a848a
Arina Vasiljeva <mistclick.a@gmail.com>
2025-01-07 07:07:35 +0100

    implement basic UI for the profile page

--- apps/web/src/app/(no-search-bar)/profile/page.tsx
+++ apps/web/src/app/(no-search-bar)/profile/page.tsx
@@ -1,5 +1,6 @@
 import { notFound } from "next/navigation";
 import { Avatar, AvatarFallback, AvatarImage } from "@repo/ui/avatar";
+import { Input } from "@repo/ui/input";
 
 import { auth } from "~/server/auth";
 
@@ -14,8 +15,28 @@ const ProfilePage = async () => {
     <main className="flex h-full w-full divide-x-2 divide-accent/55 !px-14">
       <div className="flex-1 pl-20 pr-6">place sections here</div>
       <div className="flex-[3] pl-6 pr-20">
-        <div className="flex justify-between">
-          <div>Inputs</div>
+        <div className="flex items-center justify-between gap-16">
+          <div className="w-full space-y-4 [&>*>label]:font-medium">
+            <div>
+              <label htmlFor="name">Name</label>
+              <Input
+                id="name"
+                name="name"
+                defaultValue={session.user.name}
+                className="w-full"
+              />
+            </div>
+            <div>
+              <label htmlFor="email">E-mail</label>
+              <Input
+                id="email"
+                name="email"
+                type="email"
+                defaultValue={session.user.email}
+                className="w-full"
+              />
+            </div>
+          </div>
           <div>
             <Avatar className="size-40 transition-shadow hover:shadow">
               <AvatarImage
