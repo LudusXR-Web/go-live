c630d3901272f232bea24bb6478c188d1b9a1a32
Arina Vasiljeva <mistclick.a@gmail.com>
2025-01-08 11:27:34 +0100

    implement function to delete S3 objects

--- apps/web/src/server/api/routers/s3.ts
+++ apps/web/src/server/api/routers/s3.ts
@@ -1,19 +1,31 @@
 import z from "zod";
 
-import { createTRPCRouter, publicProcedure } from "../trpc";
+import { createTRPCRouter, protectedProcedure } from "../trpc";
 import { s3 } from "~/server/aws/s3";
-import { GetObjectCommand } from "@aws-sdk/client-s3";
+import { DeleteObjectCommand, GetObjectCommand } from "@aws-sdk/client-s3";
 import { env } from "~/env";
 
 const s3Router = createTRPCRouter({
-  getObjectByKey: publicProcedure.input(z.string()).query(async ({ input }) => {
-    return await s3.send(
-      new GetObjectCommand({
-        Bucket: env.NEXT_PUBLIC_AWS_BUCKET_NAME,
-        Key: input,
-      }),
-    );
-  }),
+  getObjectByKey: protectedProcedure
+    .input(z.string())
+    .query(async ({ input }) => {
+      return await s3.send(
+        new GetObjectCommand({
+          Bucket: env.NEXT_PUBLIC_AWS_BUCKET_NAME,
+          Key: input,
+        }),
+      );
+    }),
+  deleteObjectByKey: protectedProcedure
+    .input(z.string())
+    .query(async ({ input }) => {
+      return await s3.send(
+        new DeleteObjectCommand({
+          Bucket: env.NEXT_PUBLIC_AWS_BUCKET_NAME,
+          Key: input,
+        }),
+      );
+    }),
 });
 
 export default s3Router;
