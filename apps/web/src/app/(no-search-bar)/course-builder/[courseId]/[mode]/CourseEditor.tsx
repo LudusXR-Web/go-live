6cb52a5471b48c9e1ab405f30eb113bfdd113a24
Arina Vasiljeva <ari@mistclick.me>
2025-02-11 14:22:37 +0100

    Implemented editing of separate course sections via tabs

--- apps/web/src/app/(no-search-bar)/course-builder/[courseId]/[mode]/CourseEditor.tsx
+++ apps/web/src/app/(no-search-bar)/course-builder/[courseId]/[mode]/CourseEditor.tsx
@@ -3,11 +3,18 @@
 import { createId } from "@paralleldrive/cuid2";
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
+import {
+  CircleArrowRightIcon,
+  CirclePlusIcon,
+  TrashIcon,
+  Undo2Icon,
+} from "lucide-react";
 import { Tabs, TabsList, TabsTrigger } from "@repo/ui/tabs";
+import { Button } from "@repo/ui/button";
+import { Input } from "@repo/ui/input";
 
 import { type CourseContent, type CourseSection } from "~/server/db/schema";
-import { Button } from "@repo/ui/button";
-import { CircleArrowRight, CirclePlusIcon, PlusIcon } from "lucide-react";
+import { useEffect, useState } from "react";
 
 type CourseContentStoreState = {
   sections: CourseSection[];
@@ -15,7 +22,8 @@ type CourseContentStoreState = {
 };
 
 type CourseContentStoreActions = {
-  addSection: () => void;
+  createSection: () => void;
+  deleteSection: (id: string) => void;
 };
 
 type CourseContentStore = CourseContentStoreState & CourseContentStoreActions;
@@ -23,10 +31,10 @@ type CourseContentStore = CourseContentStoreState & CourseContentStoreActions;
 export const useCourseContent = create<CourseContentStore>()(
   persist(
     (set) => ({
-      sections: [],
-      elements: [],
+      sections: [] as CourseSection[],
+      elements: [] as CourseContent[],
 
-      addSection: () =>
+      createSection: () =>
         set((state) => ({
           sections: [
             ...state.sections,
@@ -37,6 +45,10 @@ export const useCourseContent = create<CourseContentStore>()(
             },
           ],
         })),
+      deleteSection: (id) =>
+        set((state) => ({
+          sections: state.sections.filter((s) => s.id !== id),
+        })),
     }),
     {
       name: "course-content",
@@ -46,33 +58,68 @@ export const useCourseContent = create<CourseContentStore>()(
 
 const CourseEditor: React.FC = () => {
   const courseContent = useCourseContent();
+  const [tab, setTab] = useState("sections");
 
   return (
-    <Tabs className="space-y-3">
-      <div>
-        <Button
-          variant="outline"
-          className="px-3 py-1.5 hover:bg-slate-200/50"
-          onClick={() => {
-            courseContent.addSection();
-          }}
-        >
-          <CirclePlusIcon />
-        </Button>
-      </div>
+    <Tabs value={tab} onValueChange={setTab} className="space-y-3">
+      <TabsList disableDefaultStyles>
+        {tab === "sections" ? (
+          <Button
+            variant="outline"
+            className="px-3 py-1.5 hover:bg-slate-200/50"
+            onClick={() => {
+              courseContent.createSection();
+            }}
+          >
+            <span className="sr-only">Create new section</span>
+            <CirclePlusIcon />
+          </Button>
+        ) : (
+          <TabsTrigger
+            asChild
+            disableDefaultStyles
+            value="sections"
+            className="cursor-pointer"
+          >
+            <Button
+              variant="outline"
+              className="px-3 py-1.5 hover:bg-slate-200/50"
+            >
+              <span className="sr-only">Return to the list of sections</span>
+              <Undo2Icon />
+            </Button>
+          </TabsTrigger>
+        )}
+      </TabsList>
       <TabsList disableDefaultStyles className="flex flex-col gap-y-2">
-        {courseContent.sections.map((section) => (
+        {courseContent.sections.map((section, idx) => (
           <div
             key={section.id}
-            className="flex items-center justify-between rounded border px-3 py-2.5 font-medium"
+            className="flex items-center justify-between gap-3 rounded border px-3 py-2.5 font-medium"
           >
-            {section.title}
+            <Input
+              maxLength={140}
+              placeholder={`Section ${idx + 1}`}
+              className="placeholder:italic"
+            />
+            <button
+              onClick={() => courseContent.deleteSection(section.id)}
+              className="cursor-pointer rounded-md p-1.5 text-red-400 transition-colors hover:bg-red-100"
+            >
+              <span className="sr-only">Delete section {section.title}</span>
+
+              <TrashIcon />
+            </button>
             <TabsTrigger
+              asChild
               disableDefaultStyles
               value={section.id}
               className="cursor-pointer"
             >
-              <CircleArrowRight />
+              <button className="rounded-md p-1.5 transition-colors hover:bg-slate-200/50">
+                <span className="sr-only">Edit section {section.title}</span>
+                <CircleArrowRightIcon />
+              </button>
             </TabsTrigger>
           </div>
         ))}
